71d2a91a1ae95a1b1c38df9733ade8181e1b46de
Casey Rowlands <caseyrowlands@Caseys-MacBook-Pro.local>
2025-01-01 17:05:22 -0500

    added loading and not found pages, as well as mobile sheet menu

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -8,23 +8,27 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuContent,
-  DropdownMenuCheckboxItem
+  DropdownMenuCheckboxItem,
 } from '@/components/ui/dropdown-menu';
 import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 const ModeToggle = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => setMounted(true), []);
+
 
   if (!mounted) return null;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className='focus-visible:ring-0 focus-visible:ring-offset-0'>
+        <Button
+          variant="ghost"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
           {theme === 'system' ? (
             <SunMoon />
           ) : theme === 'dark' ? (
@@ -40,23 +44,20 @@ const ModeToggle = () => {
         <DropdownMenuCheckboxItem
           checked={theme === 'system'}
           onClick={() => setTheme('system')}
-          className="w-full"
         >
-          <SunMoon className='mr-2' /> System
+          <SunMoon className="mr-2" /> System
         </DropdownMenuCheckboxItem>
         <DropdownMenuCheckboxItem
           checked={theme === 'light'}
           onClick={() => setTheme('light')}
-          className="w-full"
         >
-          <SunIcon className='mr-2' /> Light
+          <SunIcon className="mr-2" /> Light
         </DropdownMenuCheckboxItem>
         <DropdownMenuCheckboxItem
           checked={theme === 'dark'}
           onClick={() => setTheme('dark')}
-          className="w-full"
         >
-          <MoonIcon className='mr-2' /> Dark
+          <MoonIcon className="mr-2" /> Dark
         </DropdownMenuCheckboxItem>
       </DropdownMenuContent>
     </DropdownMenu>
