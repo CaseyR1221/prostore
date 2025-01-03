25e06adc69b4b9fa80c5f60e9e0a84b2a7d95752
Casey Rowlands <caseyrowlands@Caseys-MacBook-Pro.local>
2025-01-03 10:19:46 -0500

    created product cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 const Homepage = () => {
   return (
-    <div>
-      Prostore
-    </div>
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+    </>
   );
-}
+};
 
 export default Homepage;
