a9da4263cd6e286ec5ba4498e4c146369a8317b1
Casey Rowlands <caseyrowlands@Caseys-MacBook-Pro-2.local>
2025-02-02 16:49:57 -0500

    added callbackUrl logic to the sign in page and the form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -17,13 +17,19 @@ export const metadata: Metadata = {
   title: 'Sign In',
 };
 
-const SignIn = async () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
   const session = await auth();
 
   if (session) {
-    return redirect('/');
+    return redirect(callbackUrl || '/');
   }
-  
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
@@ -50,4 +56,4 @@ const SignIn = async () => {
   );
 };
 
-export default SignIn;
+export default SignInPage;
