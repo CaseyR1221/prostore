7fd87f46e6739264fb7faa601bbadf5aac4a2626
Casey Rowlands <caseyrowlands@Caseys-MacBook-Pro-2.local>
2025-02-14 22:37:31 -0500

    added sessionCartID to cookiw to keep track of users cart, and worked more on addItemToCart and getMyCart actions

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { compareSync } from 'bcrypt-ts-edge';
 import NextAuth from 'next-auth';
 import type { NextAuthConfig } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { NextResponse } from 'next/server';
 
 import { prisma } from '@/db/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
@@ -60,7 +61,7 @@ export const config = {
       session.user.id = token.sub;
       session.user.name = token.name;
       session.user.role = token.role;
-      
+
       if (trigger === 'update') {
         session.user.name = user.name;
       }
@@ -89,6 +90,31 @@ export const config = {
       }
 
       return token;
+    },
+    authorized({ request }: any) {
+      // Check for cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set the newly generated sessionCartId in the response cookies
+        response.cookies.set('sessionCartId', sessionCartId);
+
+        // Return the response with the sessionCartId set
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -3,7 +3,6 @@
 import { Button } from '@/components/ui/button';
 import { ToastAction } from '@/components/ui/toast';
 import { useToast } from '@/hooks/use-toast';
-import { round2 } from '@/lib/utils';
 import { CartItem } from '@/types';
 import { Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
